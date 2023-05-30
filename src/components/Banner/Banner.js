decc6c4784de7e44804724b87794cbe03993fa2a
Ajay v <vajay4834@gmail.com>
2023-05-30 20:22:03 +0530

    Banner is no dynamic

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,22 +1,42 @@
-import React from 'react'
-import './Banner.css'
-function Banner() {
-    return (
-        <div className='banner'>
-            <div className='content'>
-                <h1 className='title'>
-                    Movie Name
-                </h1>
-                <div className='banner_buttons'>
-                    <button className='button'>Play</button>
-                    <button className='button'>My List</button>
-                </div>
-                <h1 className='description'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. </h1>
-            </div>
-          <div className="fade_bottom"></div>
+import React, { useEffect, useState } from 'react';
+import './Banner.css';
+import { API_KEY, imageurl } from '../Constents/constats';
+import axios from '../../axios';
 
+function Banner() {
+    const [movie, setMovie] = useState(null);
+
+    useEffect(() => {
+        axios
+            .get(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`)
+            .then((response) => {
+                setMovie(response.data.results[0]);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    return (
+        <div
+      className="banner"
+      style={{
+        backgroundImage: `url(${movie ? `https://image.tmdb.org/t/p/original${movie.backdrop_path}` : 'loading'})`,
+      }}
+    >
+            <div className="content">
+                <h1 className="title">{movie ? movie.title : 'Loading...'}</h1>
+                <div className="banner_buttons">
+                    <button className="button">Play</button>
+                    <button className="button">My List</button>
+                </div>
+                <h1 className="description">
+                    {movie ? movie.overview : "Loading"}
+                </h1>
+            </div>
+            <div className="fade_bottom"></div>
         </div>
-    )
+    );
 }
 
-export default Banner
+export default Banner;
---
+++ src/components/Constents/constats.js
@@ -0,0 +1,3 @@
+export const baseUrl='https://api.themoviedb.org/3';
+export const API_KEY="3522a8f8d1c3332e1a9bb409de4df75e";
+export const imageurl='https://image.tmdb.org/t/p/original';
