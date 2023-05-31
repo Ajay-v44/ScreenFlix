17156894243abdb8e2e71658d921a96415ec704a
Ajay v <vajay4834@gmail.com>
2023-05-31 15:28:41 +0530

    Added youtube vedios using npm mdule

--- src/components/RowPost/RowPost.js
+++ src/components/RowPost/RowPost.js
@@ -1,30 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import YouTube from 'react-youtube';
 import './RowPost.css';
 
-function RowPost() {
+function RowPost(props) {
   const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
-    const API_KEY = '3522a8f8d1c3332e1a9bb409de4df75e'; // Replace with your actual API key
+    const API_KEY = '3522a8f8d1c3332e1a9bb409de4df75e'; // Replace with your actual TMDB API key
+
+    let url = `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`;
+    if (props.title === 'Action') {
+      url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc'; // Replace with the actual API URL for Tamil action movies
+    }
 
     axios
-      .get(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`)
+      .get(url)
       .then((response) => {
         setMovies(response.data.results);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [props.title]);
+
+  const handleMovieClick = (movie) => {
+    setSelectedMovie(movie);
+  };
+
+  const closeVideoPlayer = () => {
+    setSelectedMovie(null);
+  };
+
+  const opts = {
+    height: '360',
+    width: '640',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
   return (
     <div className='row'>
-      <h2 className='title'>ScreenFlix Originals</h2>
+      <h2 className='title'>{props.title}</h2>
       <div className='posters'>
         {movies.map((movie) => (
-          <div key={movie.id} className='poster'>
-            <img className='poster'
+          <div key={movie.id} className='poster' onClick={() => handleMovieClick(movie)}>
+            <img
+              className={props.isSmall ? 'smallposter' : 'poster'}
               src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
               alt='Movie Poster'
             />
@@ -35,6 +59,14 @@ function RowPost() {
           </div>
         ))}
       </div>
+      {selectedMovie && (
+        <div className='video-player'>
+          <YouTube videoId={selectedMovie.videoId} opts={opts} />
+          <button className='close-button' onClick={closeVideoPlayer}>
+            Close
+          </button>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/url.js
@@ -0,0 +1,5 @@
+
+ import {API_KEY} from './components/Constents/constats'
+ export const orginals='https://image.tmdb.org/t/p/original${movie.backdrop_path}'
+ export const actions='/3/discover/movie?api_key=${API_KEY}&with_genres=28'
+ 
